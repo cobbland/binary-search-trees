bfe5e5df5684c66c725ea2edb22a933ec6b12ce9
cobbland <jacobdensford@proton.me>
2025-03-15 15:06:24 -0400

    Add Node class

--- Node.js
+++ Node.js
@@ -1,3 +1,9 @@
 export default class Node {
-    
+
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
 }
