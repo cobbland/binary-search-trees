0d90bcf7c71fe16a9dc0d8a403ca0b7bb883ed85
cobbland <jacobdensford@proton.me>
2025-03-15 15:12:36 -0400

    Start work on Node class

--- Tree.js
+++ Tree.js
@@ -2,4 +2,13 @@ import Node from "./Node.js";
 
 export default class Tree {
 
+    constructor(arr) {
+        this.arr = arr;
+        this.root = this.buildTree(arr);
+    }
+
+    buildTree(arr) {
+        return new Node(arr[1]); // testing...
+    }
+
 }
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import Tree from "./Tree.js";
+
+const test = new Tree([1,2,3]);
+
+console.log(test.root);
