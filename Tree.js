36fad19b228da68997eef5414f095d06097e9971
cobbland <jacobdensford@proton.me>
2025-03-17 09:23:49 -0400

    Add buildTree function

--- Tree.js
+++ Tree.js
@@ -4,11 +4,27 @@ export default class Tree {
 
     constructor(arr) {
         this.arr = arr;
-        this.root = this.buildTree(arr);
+        this.root = this.buildTree(this.arr);
     }
 
-    buildTree(arr) {
-        return new Node(arr[1]); // testing...
+    /*
+    Write a buildTree(array) function that takes an array of data 
+    (e.g., [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into 
+    a balanced binary tree full of Node objects appropriately placed 
+    (don’t forget to sort and remove duplicates!). 
+    The buildTree function should return the level-0 root node.
+    */
+    buildTree(arr, start = 0, end = arr.length - 1) {
+        if (start > end) {
+            return null;
+        }
+        const mid = Math.floor((start + end) / 2) // does that work to calculate the middle of the array?
+        const root = new Node(arr[mid]);
+        root.left = this.buildTree(arr, start, mid - 1);
+        root.right = this.buildTree(arr, mid + 1, end);
+        return root;
     }
+
+
 
 }
--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 import Tree from "./Tree.js";
 
-const test = new Tree([1,2,3]);
+const test = new Tree([1, 2, 3, 4, 5]);
 
 console.log(test.root);
+
+/*
+Tip: If you would like to visualize your binary search tree, 
+here is a prettyPrint() function that will console.log your tree 
+in a structured format. This function will expect to receive 
+the root of your tree as the value for the node parameter.
+*/
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+prettyPrint(test.root);
